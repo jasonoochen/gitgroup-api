227bdc6b8f9f2033f6333f762d0b9a1a0d865740
XuyingC <43390730+XuyingC@users.noreply.github.com>
2018-11-06 00:10:11 -0500

    test_Xuying

--- lib/tests/models/userModel.test.ts
+++ lib/tests/models/userModel.test.ts
@@ -2,7 +2,7 @@ import { User } from "../../models/userModel";
 
 describe("test user class constructor", () => {
   it("should create a new user object with basic name and id", () => {
-    const user: User = new User("test name", "1");
+    const user: User = new User("1", "test name"); //<-"test name", "1"
     expect(user).toMatchObject({
       id: "1",
       name: "test name"
